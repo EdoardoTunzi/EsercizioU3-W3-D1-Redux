6fa9c00609bb4ac437d176cf730613156da17911
Edoardo <edfl.tunzi@gmail.com>
2024-12-09 16:31:18 +0100

    company name on the fav list is now linked to company all job listing page.

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,17 +1,25 @@
-import { ListGroup } from "react-bootstrap";
+import { Col, ListGroup, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Favourites = () => {
   const favourites = useSelector((state) => state.favourites);
   console.log(favourites);
   return (
     <>
-      <h1>Favourites company List</h1>
-      <ListGroup>
-        {favourites.map((company, index) => (
-          <ListGroup.Item key={index}>{company}</ListGroup.Item>
-        ))}
-      </ListGroup>
+      <Row className="justify-content-center">
+        <Col xs={4}>
+          <h1>Favourites companies list</h1>
+          <ListGroup className="my-3">
+            {favourites.map((company, index) => (
+              <ListGroup.Item key={index}>
+                <Link to={`/${company}`}>{company}</Link>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+          <Link to="/">Back to homepage</Link>
+        </Col>
+      </Row>
     </>
   );
 };
